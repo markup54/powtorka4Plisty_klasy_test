598d2c47008fee3e5013baf10fdabef0262390d2
markup54 <marzenakupka@tlen.pl>
2023-03-03 08:28:21 +0100

    klasa Uczen

--- konsola/src/Uczen.java
+++ konsola/src/Uczen.java
@@ -8,5 +8,31 @@ public class Uczen extends Osoba{
     public Uczen(String imie, String nazwisko) {
         super(imie, nazwisko);
         this.oceny = new ArrayList<>();
+        licznik++;
+    }
+    public void sprawdzObecnosc(int o){
+        switch (o){
+            case 0:
+                obecnosc = Obecnosc.OBECNY;
+                break;
+            case 1:
+                obecnosc = Obecnosc.NIEOBECNY;
+                break;
+            case 2:
+                obecnosc = Obecnosc.SPOZNIONY;
+                break;
+        }
+    }
+    public void dopiszOcene (double ocena){
+        oceny.add(ocena);
+    }
+    public double obliczSrednia(){
+        double s = 0;
+        for(double ocena: oceny){
+            s = s+ocena;
+        }
+        if (oceny.size()>0)
+            return s/ oceny.size();
+        return 0;
     }
 }
