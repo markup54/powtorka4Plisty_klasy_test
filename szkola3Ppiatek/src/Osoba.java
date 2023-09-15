a6d7e4f0219310b5298f9a9b43d143a557152c98
markup54 <marzenakupka@tlen.pl>
2023-09-15 08:10:11 +0200

    klasa Osoba prywatne wlasnosci przeciazanie konstruktora gettery i settery

--- szkola3Ppiatek/src/Osoba.java
+++ szkola3Ppiatek/src/Osoba.java
@@ -1,4 +1,38 @@
 public class Osoba {
-    public String imie;
-    public int wiek;
+    private String imie;
+    private int wiek;
+    //hermetyzacja
+
+    public Osoba() {
+    }
+
+    public Osoba(String imie) {
+        this.imie = imie;
+        this.wiek =7;
+    }
+
+    public Osoba(String imie, int wiek) {
+        this.imie = imie;
+        this.wiek = wiek;
+    }
+    //przeciążanie konstrktora
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        if (wiek < 0) {
+            this.wiek = 0;
+        } else
+            this.wiek = wiek;
+    }
 }
