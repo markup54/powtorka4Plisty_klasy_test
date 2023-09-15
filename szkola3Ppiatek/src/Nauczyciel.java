00eab12e91af4ede1fc5f5d05647fbf6d6dac384
markup54 <marzenakupka@tlen.pl>
2023-09-15 08:54:19 +0200

    Klasa Nauczyciel

--- szkola3Ppiatek/src/Nauczyciel.java
+++ szkola3Ppiatek/src/Nauczyciel.java
@@ -1,2 +1,25 @@
-public class Nauczyciel extends Osoba{
+import java.util.ArrayList;
+
+public sealed class Nauczyciel extends Osoba permits Wychowawca{
+    private ArrayList<String> przedmioty = new ArrayList<>();
+
+    public Nauczyciel(String imie, int wiek ,String przedmiot) {
+        super(imie, wiek);
+        przedmioty.add(przedmiot);
+    }
+
+    public Nauczyciel(String imie, int wiek, String ...przedmioty) {
+        super(imie, wiek);
+        for (String przedmiot:
+             przedmioty) {
+            this.przedmioty.add(przedmiot);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"Nauczyciel{" +
+                "przedmioty=" + przedmioty +
+                "} " ;
+    }
 }
