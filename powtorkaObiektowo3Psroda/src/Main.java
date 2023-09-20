2bf0ef9c060a6541d50bda4a595d06a8a40d8e7f
markup54 <marzenakupka@tlen.pl>
2023-09-20 16:50:09 +0200

    klasa osoba prawie gotowa

--- powtorkaObiektowo3Psroda/src/Main.java
+++ powtorkaObiektowo3Psroda/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Liczba zarejestrowanych osób "+Osoba.licznikKlasy);
+        Osoba osoba =new Osoba();
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("podaj id osoby");
+        int id =klawiatura.nextInt();
+        System.out.println("Podaj imię osoby");
+        String imie = klawiatura.next();
+        Osoba osoba1 = new Osoba(id,imie);
+        Osoba osoba2 = new Osoba(osoba1);
+        osoba.wypiszImie("Jan");
+        osoba1.wypiszImie("Jan");
+        osoba2.wypiszImie("Jan");
+        System.out.println("Liczba zarejestrowanych osób "+Osoba.licznikKlasy);
     }
 }
---
+++ powtorkaObiektowo3Psroda/src/Osoba.java
@@ -0,0 +1,25 @@
+public class Osoba {
+    private int id;
+    private String imieOsoby;
+    public static int licznikKlasy =0;
+
+    public Osoba() {
+        this.id = 0;
+        this.imieOsoby = "";
+        licznikKlasy++;
+    }
+
+    public Osoba(int id, String imieOsoby) {
+        this.id = id;
+        this.imieOsoby = imieOsoby;
+        licznikKlasy++;
+    }
+    public Osoba(Osoba osoba){
+        this.id = osoba.id;
+        this.imieOsoby = osoba.imieOsoby;
+        licznikKlasy++;
+    }
+    public void wypiszImie(String inneImie){
+        System.out.println("Cześć "+inneImie+" mam na imię "+imieOsoby);
+    }
+}
